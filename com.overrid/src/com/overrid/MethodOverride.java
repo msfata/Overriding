9a7597fae7d0d11c5843ead5bbf5884885cb679d
44742 <44742@ProgramminHelp>
2019-05-12 18:59:31 +0100

    commit updates

--- com.overrid/src/com/overrid/MethodOverride.java
+++ com.overrid/src/com/overrid/MethodOverride.java
@@ -22,10 +22,17 @@ public class MethodOverride {
 		System.out.println(c.run("hi"));
 
 		/*
-		 * indication method overriding
+		 * indication method overriding upcasting
 		 */
 		Parent pc = new Child();
 		System.out.println(pc.run("child run overrids parent run"));
+
+		/*
+		 * downcasting example
+		 */
+		Parent pa = new Child();
+		Child ca = (Child) pa;
+		System.out.println(ca.run("downcasting example"));
 	}
 }
 
